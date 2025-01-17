ea631c448f374458c1d93bfeaf665fa5ee6a8d4a
Nikhil Sheoran <thenikhilsheoran@gmail.com>
2025-01-18 01:05:44 +0530

    for utkarsh's openai funciton calling

--- actions/serverActions.ts
+++ actions/serverActions.ts
@@ -1,6 +1,10 @@
 "use server";
 
+//initialize openai
 export async function startChat(): Promise<string> {
+
+// write openai code using structured outputs. return json.stringify(response)
+    
     return new Promise((resolve) => {
         setTimeout(() => {
             resolve("Ye chatgpt response hai");
